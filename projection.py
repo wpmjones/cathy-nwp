675a9bc23ff18970f5283e296c940241132426fd
Patrick <wpmjones@gmail.com>
2025-08-05 18:58:48 -0700

    update projection.py

--- projection.py
+++ projection.py
@@ -4,8 +4,8 @@ import gspread
 from loguru import logger
 
 # Connect to Google Sheets
-gc = gspread.oauth(credentials_filename=creds.gspread,
-                   authorized_user_filename=creds.gspread_user)
+gc = gspread.oauth(credentials_filename="C:\Scans\credentials.json",
+                   authorized_user_filename="C:\Scans\\authorized_user.json")
 spreadsheet = gc.open_by_key(creds.schedule_id)
 sheet = spreadsheet.worksheet("6.16-6.21")
 
